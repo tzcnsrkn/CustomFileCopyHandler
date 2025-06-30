fa56ddafb8c35ed4bfa598a328b7133b0900e088
tzcnsrkn <serkan.tezcan.ce@gmail.com>
2025-06-30 22:54:35 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] fileNames = {"FileA.txt", "FileB.txt", "FileC.txt"};
+string targetPath = @"/mnt/destination";
+
+foreach (var fileName in fileNames)
+{
+    File.Copy(@$"files/{fileName}", @$"{targetPath}/{fileName}", true);
+}
+Console.WriteLine("Files are succesfully copied.");
+
+Thread.Sleep(300000);
